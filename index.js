6ae75e732593a2b7ea980045bd837bb1ef7d78db
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2022-01-03 02:36:22 +0600

    update

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 require('dotenv').config();
+const cors = require('cors');
 const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2qgak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
